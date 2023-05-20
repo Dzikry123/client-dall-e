aa1b1088501034e56369ffe8267bf017d1caae51
Dzikry123 <96764302+Dzikry123@users.noreply.github.com>
2023-05-20 10:57:13 +0700

    Update CreatePost.jsx

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch(https://server-dall-e.vercel.app/api/v1/dalle', {
+        const response = await fetch('https://server-dall-e.vercel.app/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -57,8 +57,8 @@ const CreatePost = () => {
           headers: {
             'Content-type': 'application/json',
           },
-          body: JSON.stringify(form),
-        })
+          body: JSON.stringify({ ...form }),
+        });
 
         await response.json();
         navigate('/')
